d72042b03fdde45e122fab8d4a7fa2dd1ef3e067
John Paul Pacatang <59640442+JohnPaulPacatang@users.noreply.github.com>
2023-11-06 17:08:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,220 @@
-function scrollToElement(elementSelector, instance = 0) {
-  const elements = document.querySelectorAll(elementSelector);
+// === NAVBAR ACTIVE AND RESPONSIVE START === //
+var menuBtn = document.querySelector(".menu-btn")
+var menu = document.querySelector(".nav-links")
+var menuLinks = document.querySelectorAll(".nav-links li a")
+
+menuBtn.addEventListener("click", activeClass)
+
+function activeClass() {
+  menuBtn.classList.toggle("active")
+  menu.classList.toggle("active")
+}
+
+for (i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener("click", menuItemClicked)
+}
+
+function menuItemClicked() {
+  menuBtn.classList.remove("active")
+  menu.classList.remove("active")
+}
+
+var homeSection = document.querySelector(".home")
+window.addEventListener("scroll", scrollFunction)
+window.addEventListener("load", scrollFunction)
+
+function scrollFunction() {
+  if (window.scrollY > 60) {
+    homeSection.classList.add("active")
+  } else {
+    homeSection.classList.remove("active")
+  }
+}
+// === NAVBAR ACTIVE AND RESPONSIVE END === //
+
+
+// === NAVBAR SCROLL START === //
+function scrollToElement(elementSelector, instance = 0, duration = 1000) {
+  const elements = document.querySelectorAll(elementSelector)
+
   if (elements.length > instance) {
-    elements[instance].scrollIntoView({ behavior: "smooth" });
+    const targetElement = elements[instance]
+    const targetPosition = targetElement.getBoundingClientRect().top
+    const startPosition = window.scrollY
+    let startTime = null
+
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime
+      const timeElapsed = currentTime - startTime
+      const scrollProgress = Math.min(timeElapsed / duration, 1)
+      window.scrollTo(0, startPosition + targetPosition * scrollProgress)
+
+      if (scrollProgress < 1) {
+        requestAnimationFrame(animation)
+      }
+    }
+
+    requestAnimationFrame(animation)
   }
 }
 
-const link0 = document.getElementById("link0");
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-const link4 = document.getElementById("link4");
 
+const link0 = document.getElementById("link0")
+const link1 = document.getElementById("link1")
+const link2 = document.getElementById("link2")
+const link3 = document.getElementById("link3")
+const link4 = document.getElementById("link4")
 
 link0.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
-  });
-});
+  })
+})
 
 link1.addEventListener("click", () => {
-  scrollToElement(".services-container");
-});
-
-link2.addEventListener("click", () => {
-  scrollToElement(".skills");
-});
-
-link3.addEventListener("click", () => {
-  scrollToElement(".projects");
-});
-
-link4.addEventListener("click", () => {
-  scrollToElement(".column");
-});
-
-//for loadersheesh
-document.body.style.overflow = 'hidden';
-
-window.addEventListener('load', function() {
-  setTimeout(function() {
-    var loadingOverlay = document.querySelector('.loading-overlay');
-    loadingOverlay.classList.add('hidden');
-
-    setTimeout(function() {
-      document.body.style.overflow = 'auto';
-  }, 2000);
-
+  const profileElement = document.querySelector(".profile")
+  if (profileElement) {
+    const yOffset = -50
     window.scrollTo({
-      top: 0,
-    });
-  }, 2000);
-});
-
-//For Certificates 
-$('.certificates-container').owlCarousel({
-  loop:true,
-  autoplay:true,
-  autoplayTime:6000,
-  margin:10,
-  nav:true,
-  navText:["<i class='fa-solid fa-arrow-left'></i>",
-           "<i class='fa-solid fa-arrow-right'></i>"],
-  responsive:{
-      0:{
-          items:1,
-          nav:false
-      },
-      600:{
-          items:1,
-          nav:true
-      },
-      768:{
-          items:2
-      }
+      top: profileElement.offsetTop + yOffset,
+      behavior: "smooth",
+    })
   }
 })
 
+link2.addEventListener("click", () => {
+  const projectsElement = document.querySelector(".skills")
+  if (projectsElement) {
+    const yOffset = -50
+    window.scrollTo({
+      top: projectsElement.offsetTop + yOffset,
+      behavior: "smooth",
+    })
+  }
+})
+
+link3.addEventListener("click", () => {
+  const projectsElement = document.querySelector(".projects")
+  if (projectsElement) {
+    const yOffset = -50
+    window.scrollTo({
+      top: projectsElement.offsetTop + yOffset,
+      behavior: "smooth",
+    })
+  }
+})
+
+link4.addEventListener("click", () => {
+  const projectsElement = document.querySelector(".contact")
+  if (projectsElement) {
+    const yOffset = -50
+    window.scrollTo({
+      top: projectsElement.offsetTop + yOffset,
+      behavior: "smooth",
+    })
+  }
+})
+// === NAVBAR SCROLL END === //
 
 
+// === LOADERS SHEEESH START=== //
+document.body.style.overflow = 'hidden'
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    var loadingOverlay = document.querySelector('.loading-overlay')
+    loadingOverlay.classList.add('loading-overlay-animation')
+    setTimeout(function() {
+      loadingOverlay.classList.add('hidden')
+      document.body.style.overflow = 'auto'
+    }, 1000)
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }, 2000)
+})
+// === LOADERS END === //
 
 
+// === CERTIFICATES START === //
+let filterItems = document.querySelector('.items-links')
+let filterImages = document.querySelectorAll('.cert-img')
+
+window.addEventListener('load', () => {
+  filterItems.addEventListener('click', (selectedItem) => {
+    if (selectedItem.target.classList.contains('item-link')) {
+      document.querySelector('.menu-active').classList.remove('menu-active')
+
+      selectedItem.target.classList.add('menu-active')
+
+      let filterName = selectedItem.target.getAttribute('data-name')
+
+      filterImages.forEach((image) => {
+        let filterImages = image.getAttribute('data-name')
+        if (filterImages === filterName || filterName === 'all') {
+          image.style.display = 'block'
+        } else {
+          image.style.display = 'none'
+        }
+      })
+    }
+  })
+})
+// === CERTIFICATES END === //
 
 
+// === TESTIMONIALS SCROLL START === //
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500, 
+  },
+})
+// === TESTIMONIALS SCROLL END === //
+
+// / === CIRCLE CURSOR START === //
+const cursorOutline = document.querySelector("[data-cursor-outline]")
+
+window.addEventListener("mousemove", function (e) {
+
+  const posX = e.clientX
+  const posY = e.clientY
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, {duration: 500, fill: "forwards"})
+
+})
+// === CIRCLE CURSOR END === //
+
+// === CLOCK START === //
+function updateClock() {
+  const clockElement = document.getElementById("digital-clock")
+  const now = new Date()
+  
+  let hours = now.getHours()
+  const amPm = hours >= 12 ? 'PM': 'AM'
+  hours = hours % 12 || 12; 
+
+  const minutes = now.getMinutes().toString().padStart(2, "0")
+  const seconds = now.getSeconds().toString().padStart(2, "0")
+
+  const timeString = `${hours}:${minutes}:${seconds} ${amPm}`
+  clockElement.textContent = timeString
+}
+
+setInterval(updateClock, 1000)
+updateClock()
+// === CLOCK END === //
