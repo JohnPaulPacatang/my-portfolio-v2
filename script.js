0c4995168dfc6fac6d684c61e02ef02d2da44878
John Paul Pacatang <59640442+JohnPaulPacatang@users.noreply.github.com>
2024-05-20 20:44:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -165,24 +165,6 @@ window.addEventListener('load', () => {
 // === CERTIFICATES END === //
 
 
-// === TESTIMONIALS SCROLL START === //
-var swiper = new Swiper(".blog-slider", {
-  spaceBetween: 30,
-  effect: "fade",
-  loop: true,
-  mousewheel: {
-    invert: false,
-  },
-  pagination: {
-    el: ".blog-slider__pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500, 
-  },
-})
-// === TESTIMONIALS SCROLL END === //
-
 // / === CIRCLE CURSOR START === //
 const cursorOutline = document.querySelector("[data-cursor-outline]")
 
